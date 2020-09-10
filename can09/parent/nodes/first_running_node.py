952c9c6ede524a4ee4e7e2b11d867872532533a2
taikikku0314 <taikikku@Taikis-Macbook.local>
2020-09-10 17:49:26 +0900

    add hogehoge

--- can09/parent/nodes/first_running_node.py
+++ can09/parent/nodes/first_running_node.py
@@ -3,6 +3,7 @@ from typing import Dict, Any
 
 import pisat.config.dname as dname
 from pisat.core.nav import Node
+from pisat.core.logger import DataLogger
 
 import can09.parent.setting as setting
 
@@ -10,5 +11,31 @@ import can09.parent.setting as setting
 class FirstRunningNode(Node):
     
     
+    def enter(self):
+        '''
+        第一目的地が正面を向くようにその場で回転．
+        '''
+        self.dlogger: DataLogger = self.manager.get_component("DataLogger")
+    
     def judge(self, data: Dict[str, Any]) -> bool:
-        pass
+        '''
+        自分の緯度，経度がそれぞれ第一目標地点の5m以内ならTrueを返す．それ以外はFalseを返す．
+        '''
+        if data[dname.ALTITUDE] < 10:
+            print("hogehoge")
+            raise HogeHogeError(
+                "HogeHoge detected."
+            )
+        else:
+            raise HogeHogeError(
+                "HogeHoge not detected."
+            )
+    
+    
+    def control(self):
+        '''
+        走行サイクルを実行．
+        '''
+        while not self.event.is_set():
+            ref = self.dlogger.get_ref()
+            ref[0][dname.]
--- can09/parent/nodes/second_running_node.py
+++ can09/parent/nodes/second_running_node.py
@@ -1,7 +1,28 @@
 
+from typing import Dict, Any
+
+import pisat.config.dname as dname
 from pisat.core.nav import Node
+
+import can09.parent.setting as setting
 
 
 class SencondRunningNode(Node):
     
-    pass
+    
+    def enter(self):
+        '''
+        第二目的地が正面を向くようにその場で回転．
+        '''
+    
+    
+    def judge(self, data: Dict[str, Any]) -> bool:
+        '''
+        自分の緯度，経度がそれぞれ第二目標地点の5m以内ならTrueを返す．それ以外はFalseを返す．
+        '''
+    
+    
+    def control(self):
+        '''
+        遠走行サイクルを実行．
+        '''
