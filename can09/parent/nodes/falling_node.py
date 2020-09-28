9eb8d23472c8057e5050fce3e4f845a3a94b3156
ITOMIZUKI <kiatsuhan@gmail.com>
2020-09-28 22:21:15 +0900

    add deque

--- can09/parent/nodes/falling_node.py
+++ can09/parent/nodes/falling_node.py
@@ -1,5 +1,6 @@
 
 from typing import Dict, Any
+from collections import deque
 
 import pisat.config.dname as dname
 from pisat.core.nav import Node
@@ -9,11 +10,23 @@ import can09.parent.setting as setting
 
 class FallingNode(Node):
 
-    def enter(self, data: Dict[str, Any]):
+    def enter(self):
         self.dlogger: DataLogger = self.manager.get_component("DataLogger")
-        self.SEALEVEL = data[dname.ALTITUDE_SEALEVEL]
+        self.que = deque(maxlen=100)
 
     def judge(self, data: Dict[str, Any]) -> bool:
+
+        sealevel = data.get(dname.ALTITUDE_SEALEVEL)
+        if sealevel is None:
+            return False
+
+        self.que.appendleft(sealevel)
+
+        if len(self.que) < 50:
+            return False
+
+        
+
         if self.SEALEVEL - setting.THRESHOLD_LANDING_DETECT \
             and data[dname.ALTITUDE_SEALEVEL] < self.SEALEVEL + setting.THRESHOLD_LANDING_DETECT:
             print("This isn't flying, this is falling with style!")
