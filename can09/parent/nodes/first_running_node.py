89cecfcc5a598de2a97f984b8bd8bd2661c4cde1
taikikku0314 <taikikku0314@gmail.com>
2020-10-04 15:44:18 +0900

    updated

--- can09/parent/nodes/first_running_node.py
+++ can09/parent/nodes/first_running_node.py
@@ -1,6 +1,7 @@
 
 from typing import Dict, Deque
 from time import time, sleep
+from numpy import abs
 
 import pisat.config.dname as dname
 from pisat.config.type import Logable
@@ -18,8 +19,9 @@ class FirstRunningNode(Node):
         第一目的地が正面を向くようにその場で回転．
         '''
         dlogger = self.manager.get_component("DataLogger")
-        self.ref = dlogger.refqueue
-        self.motors = self.manager.get_component("TwoWheels")
+        self.ref: DataLogger = dlogger.refqueue
+        self.motor_R = self.manager.get_component("SimplePWMMotorDriver")
+        self.motor_L = self.manager.get_component("SimolePWMMotorDriver")
     
     def judge(self, data: Dict[str, Logable]) -> bool:
         '''
@@ -34,10 +36,11 @@ class FirstRunningNode(Node):
             if self.offset is None:
                 continue
             
-            if self.offset > setting.THRESHOLD_PID_START or self.offset < -setting.THRESHOOD_PID_START :
+            if abs(self.offset) > setting.THRESHOLD_PID_START:
                 self._pid_control()
             else:
-                self.motors.straight()
+                self.motor_R.ccw(setting.DEFAULT_DUTY_RATIO)
+                self.motor_L.cw(setting.DEFAULT_DUTY_RATIO)
             
     def _pid_control(self):
         self._clear()
@@ -45,13 +48,15 @@ class FirstRunningNode(Node):
         if self.offset > 0:
             while self.offset > setting.THRESHOLD_PID_FINISH:
                 self._update()
-                
+                self.motor_R.ccw(self.output)
+                self.motor_L.cw(setting.DEFAULT_DUTY_RATIO)
                 sleep(1)
             
         else:
             while self.offset < -setting.THRESHOLD_PID_FINISH:
                 self._update()
-                
+                self.motor_R.ccw(setting.DEFAULT_DUTY_RATIO)
+                self.motor_L.cw(self.output)
                 sleep(1)
             
     def _clear(self):
@@ -66,14 +71,15 @@ class FirstRunningNode(Node):
 
     def _update(self):
         que = self.ref.get()
-        self.offset = que[0].get(dname.OFFSET_ANGLE)
+        self.offset = abs(que[0].get(dname.OFFSET_ANGLE))
         
         self._current_time = time()
         delta_time = self._current_time - self._last_time
         delta_error = self.offset - self._last_offset
         
-        self._p_term = setting.KP * self.offset
-        self._i_term += self.offset * self.delta_time
+        self._p_term = self.offset
+        
+        self._i_term += self.offset * delta_time
 
         if self._i_term > setting.MAX_I_TERM:
             self._i_term = setting.MAX_I_TERM
@@ -85,4 +91,4 @@ class FirstRunningNode(Node):
         self._last_time = self._current_time
         self._last_error = self.offset
         
-        self.output = self._p_term + (setting.KI * self._i_term) + (setting.KD * self._d_term)
+        self.output = (setting.KP * self._p_term) + (setting.KI * self._i_term) + (setting.KD * self._d_term)
