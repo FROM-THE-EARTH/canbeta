9001dc3575cb843ff94c3a6106029468bd273262
taikikku0314 <taikikku0314@gmail.com>
2020-11-15 13:00:11 +0900

    added options

--- can09/parent/util/pid_controller.py
+++ can09/parent/util/pid_controller.py
@@ -1,33 +1,62 @@
+#! python 3
+
+
+"""pid_controller.py
+    
+    Requires:
+        kp                  - proportional gain (default is None)
+                                -> If given, the proportional controller is used.
+        ki                  - integral gain (default is None)
+                                -> If given, the integral controller is used.
+        kd                  - derivative gain (default is None)
+                                -> If given, the derivative controller is used.
+        max_input           - maximum desired input if any (default is None)
+        min_input           - minimum desired input if any (default is None)
+        sse_ratio           - steady-state error ratio (default is None)
+                                -> Set this if you want to use the integral controller only for
+                                   eliminating steady-state error (0.1 < sse_dtctr < 0.3 recommended).
+        threshold_i_ctrlr   - threshold intergral controller (default is None)
+                                -> Each time it is detected that it is in the range of steady-state error,
+                                   self._i_counter is incresed by 1, and when it exceeds threshold_i_cntrlr,
+                                   steady-state error is detected, and the integral controller is activated.
+
+    Returns:
+        input: float
+        
+    Notes:
+        If you put a limitation on the integral controller, you MUST give BOTH sse_ratio AND threshold_i_ctrlr.
+"""
+
+
 from time import time
+from typing import Optional
 
 class PIDController:
 
     def __init__(self,
-                 kp: float,
-                 ki: float,
-                 kd: float,
-                 max_input: float,
-                 min_input: float,
-                 acceptable_moe: float,
-                 threshold_i_ctrlr: int) -> None:
-        
-        # TODO; The integral controller's limitation should be selectable.
-        
+                 kp: Optional[float] = None,
+                 ki: Optional[float] = None,
+                 kd: Optional[float] = None,
+                 max_input: Optional[float] = None,
+                 min_input: Optional[float] = None,
+                 sse_ratio: Optional[float] = None,
+                 threshold_i_ctrlr: Optional[int] = None) -> None:
+                
         """Initializes the instance variables"""
         # Setting constants
-        self._kp: float = kp
-        self._ki: float = ki
-        self._kd: float = kd
-        self._max_input: float = max_input
-        self._min_input: float = min_input
-        self._acceptable_moe: float = acceptable_moe
-        self._threshold_i_ctrlr: int = threshold_i_ctrlr
+        self._kp: Optional[float] = kp
+        self._ki: Optional[float] = ki
+        self._kd: Optional[float] = kd
+        self._max_input: Optional[float] = max_input
+        self._min_input: Optional[float] = min_input
+        self._sse_ratio: Optional[float] = sse_ratio
+        self._threshold_i_ctrlr: Optional[int] = threshold_i_ctrlr
 
         # Initializing variables
         self._last_time: float = time()
         self._last_i_term: float = 0.
         self._last_error: float = 0.
-        self._moe: float = 0.
+        self._sse_range: float = 0.
         self._counter: int = 0
         self._i_counter: int = 0
 
@@ -38,8 +67,9 @@ class PIDController:
             # Done in order to make the first dalta_error 0
             self._last_error = error
             
-            # Calculating the value for the margin of error (moe), used in the integral controller
-            self._moe = error * self._acceptable_moe
+            # Calculating the steady-state error range, used in the integral controller
+            if self._sse_ratio is not None:
+                self._sse_range = error * self._sse_ratio
             
             # Making the counter non 0 so that it will not be activated from the next time
             self._counter += 1
@@ -50,16 +80,32 @@ class PIDController:
         delta_time = current_time - self._last_time
 
         # Calculating each term
-        p_term = self._calc_p_term(error)
-        i_term = self._calc_i_term(error, delta_time)
-        d_term = self._calc_d_term(delta_error, delta_time)
+        if self._kp is not None:
+            p_term = self._calc_p_term(error)
+        else:
+            p_term = 0.
+        
+        if self._ki is not None:
+            if self._sse_ratio is not None & self._threshold_i_ctrlr is not None:
+                i_term = self._calc_i_term_sse(error, delta_time)
+            else:
+                i_term = self._calc_i_term
+        else:
+            i_term = 0.
+            
+        if self._kd is not None:
+            d_term = self._calc_d_term(delta_error, delta_time)
+        else:
+            d_term = 0.
 
         # Calculating an input
         input = p_term + i_term + d_term
-        if input < self._min_input:
-            input = self._min_input
-        elif input > self._max_input:
-            input = self._max_input
+        if self._min_input is not None:
+            if input < self._min_input:
+                input = self._min_input
+        if self._max_input is not None:
+            if input > self._max_input:
+                input = self._max_input
 
         # Saving parameters
         self._last_error = error
@@ -71,9 +117,14 @@ class PIDController:
     def _calc_p_term(self, error: float) -> float:
         """Calculates a p_term"""
         return self._kp * error
-
+    
     def _calc_i_term(self, error: float, delta_time: float) -> float:
-        """Calcultes an i_term using trapezoidal rule or returns 0
+        """Calculates an i_term using trapezoidal rule"""
+        delta_i_term = ((self._last_error + error) / 2) * delta_time   
+        return self._ki * (self._last_i_term + delta_i_term)
+
+    def _calc_i_term_sse(self, error: float, delta_time: float) -> float:
+        """Calculates an i_term using trapezoidal rule or returns 0
         
         Note:
         -----
@@ -81,7 +132,7 @@ class PIDController:
         """
         
         # Increasing self._i_counter by 1 when the error is within the margin of error (moe)
-        if self._last_error - self._moe < error < self._last_error + self._moe:
+        if -self._sse_range < error < self._sse_range:
             self._i_counter += 1
         else:
             self._last_i_term = 0
