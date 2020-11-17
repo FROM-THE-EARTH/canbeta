4411da3209ee5e1d0eaf24bdbb6da43f0f4570c0
taikikku0314 <taikikku0314@gmail.com>
2020-11-17 20:23:56 +0900

    wrote without using manager

--- test/pid_controller_simulation/limit_sensitivity_method.py
+++ test/pid_controller_simulation/limit_sensitivity_method.py
@@ -4,25 +4,28 @@
 """
 
 from time import time, sleep
+import pigpio
 
+from pisat.core.logger import SensorController
+from pisat.handler import PigpioI2CHandler, PyserialSerialHandler, PigpioPWMHandler
 from pisat.calc import Navigator, Position
 from pisat.model import cached_loggable, LinkedDataModelBase, linked_loggable
 from pisat.sensor import SamM8Q, Bno055
 from pisat.util.deco import cached_property
-from pisat.actuator import BD62xx
-from pisat.core.logger import RefQueue
-
+from pisat.actuator import PWMDCMotorDriver
 import can09.parent.setting as setting
 from can09.parent.util import PIDController
 
+NAME_BNO055 = "bno055"
+NAME_GPS = "gps"
 
 class RunningModel(LinkedDataModelBase):
 
     TEMP_GOAL = [0., 0.]
     
-    longitude = linked_loggable(SamM8Q.DataModel.longitude, setting.NAME_GPS)
-    latitude = linked_loggable(SamM8Q.DataModel.latitude, setting.NAME_GPS)
-    mag = linked_loggable(Bno055.DataModel.mag, setting.NAME_BNO055)
+    longitude = linked_loggable(SamM8Q.DataModel.longitude, NAME_GPS)
+    latitude = linked_loggable(SamM8Q.DataModel.latitude, NAME_GPS)
+    mag = linked_loggable(Bno055.DataModel.mag, NAME_BNO055)
     
     def setup(self) -> None:
         self._navi_goal_temp = Navigator(Position(*self.TEMP_GOAL))
@@ -38,54 +41,55 @@ class RunningModel(LinkedDataModelBase):
     def offset_angle2goal(self):
         heading = self.calc_heading(self.mag)
         return self._navi_goal.delta_angle(self.position, heading)
-    
-    
-class LimitSensitivityMethod:
-    
-    model = RunningModel
-    
-    def __init__(self, kp: float, ex_time: float) -> None:
-        self._ref: RefQueue = self.manager.get_component(setting.NAME_DATA_LOGGER).refqueue
-        self._right_motor: BD62xx = self.manager.get_component(setting.NAME_MOTOR_R)
-        self._left_motor: BD62xx = self.manager.get_component(setting.NAME_MOTOR_L)
-        self._first_time: float = time()
-        self._last_time: float = self._first_time
-        self._delta_time: float = 0.
-        
-        self._ex_time: float = ex_time
-        
-        self._pidcontroller = PIDController(kp=kp)
-        
-    def exec_p_ctrl(self) -> None:
-        while self._delta_time < self._ex_time:
-            current_time = time()
-            if (current_time - self._last_time) < setting.SAMPLE_TIME:
-                continue
-            self._last_time = current_time
-                     
-            offset = self._ref.get()[0].offset_angle2child
-            if offset is None:
-                continue
-            
-            duty = self._pidcontroller.calc_input(offset)
-                
-            self._right_motor.ccw(duty)
-            self._left_motor.cw(setting.DUTY_BASE)
-            
-            self._last_time = current_time
-            self._delta_time = self._first_time - current_time
-            
-            sleep(setting.SAMPLE_TIME)
-        
-        self._right_motor.brake()
-        self._left_motor.brake()
+
 
 def main() -> None:
     kp = float(input('Enter Kp: '))
     ex_time = float(input('Enter experiment time [sec]: '))
     
-    tunner = LimitSensitivityMethod(kp, ex_time)
-    tunner.exec_p_ctrl()
+    pi = pigpio.pi()
+    
+    handler_bno055 = PigpioI2CHandler(pi, 0x28)
+    handler_gps = PyserialSerialHandler("/dev/serial0", baudrate=9600)
+    handler_motor_l_fin = PigpioPWMHandler(pi, 12, 40000)
+    handler_motor_l_rin = PigpioPWMHandler(pi, 18, 40000)
+    handler_motor_r_fin = PigpioPWMHandler(pi, 13, 40000)
+    handler_motor_r_rin = PigpioPWMHandler(pi, 19, 40000)
+
+    bno055 = Bno055(handler_bno055, name=NAME_BNO055)
+    gps = SamM8Q(handler_gps, name=NAME_GPS)
+    sencon = SensorController(RunningModel, bno055, gps)
+    data = sencon.read()
+    motor_l = PWMDCMotorDriver(handler_motor_l_fin, handler_motor_l_rin)
+    motor_r = PWMDCMotorDriver(handler_motor_r_fin, handler_motor_r_rin)
+    
+    pidcontroller = PIDController(kp=kp)
+    first_time = time()
+    last_time = first_time
+    delta_time = 0.
+    
+    while delta_time < ex_time:
+        current_time = time()
+        if (current_time - last_time) < setting.SAMPLE_TIME:
+            continue
+        last_time = current_time
+                     
+        offset = data.offset_angle2goal
+        if offset is None:
+            continue
+            
+        duty = pidcontroller.calc_input(offset)
+                
+        motor_r.ccw(duty)
+        motor_l.cw(setting.DUTY_BASE)
+            
+        last_time = current_time
+        delta_time = first_time - current_time
+            
+        sleep(setting.SAMPLE_TIME)
+        
+    motor_r.brake()
+    motor_l.brake()
     
 if __name__ == '__main__':
     main()
