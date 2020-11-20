6231be85f3bfd1fb52425c0da251a3825c840fe1
jjj999 <spaceship2021@gmail.com>
2020-11-20 23:01:06 +0900

    Updated scripts for control station

--- can09/control/listen.py
+++ can09/control/listen.py
@@ -1,3 +1,38 @@
-# TODO 
-# Write codes so that a PC in the control station can 
-# listen data from the parent or child with IM920.
+
+
+from pisat.handler import PyserialSerialHandler
+from pisat.comm.transceiver import Im920
+
+import can09.control.setting as control_setting
+import can09.setting as whole_setting
+
+
+def main():
+    handler = PyserialSerialHandler(control_setting.SERIAL_PORT, control_setting.BAUDRATE_IM920)
+    im920 = Im920(handler, name=control_setting.NAME_IM920)
+    
+    print(f"Listen Start from ID: {im920.id}", end="\n\n")    
+    
+    try:
+        while True:
+            if len(im920.buf):
+                raw = im920.recv_raw()
+                print(raw)
+                if len(raw) == 2:
+                    addr, data = raw
+                    data = data.decode()
+                    
+                    device = ""
+                    if addr == whole_setting.ADDR_IM920_CHILD:
+                        device = "CHILD"
+                    if addr == whole_setting.ADDR_IM920_PARENT:
+                        device = "PARENT"
+                        
+                    if device:
+                        print(f"from {device}: {data}")
+    except KeyboardInterrupt:
+        handler.close()
+
+
+if __name__ == "__main__":
+    main()
