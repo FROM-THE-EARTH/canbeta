9dc37797f1effbf42e8722cde42c83d985acbbcc
jjj999 <spaceship2021@gmail.com>
2020-11-20 23:47:06 +0900

    Built MissionStandbyNode.

---
+++ can09/child/nodes/mission_standby_node.py
@@ -0,0 +1,32 @@
+
+from pisat.comm.transceiver import Im920, SocketTransceiver
+from pisat.core.nav import Node
+
+import can09.setting as whole_setting
+import can09.child.setting as child_setting
+from can09.server import Request
+
+
+class MissionStandbyNode(Node):
+
+    model = None
+    
+    def enter(self) -> None:
+       self.transceiver: SocketTransceiver = self.manager.get_component(child_setting.NAME_SOCKET_TRANSCEIVER)
+
+    def judge(self, data: None) -> bool:
+        socket = self.transceiver.listen()
+        if socket is None:
+            return False
+        
+        params = Request.parse_request(socket)
+        if params.address == whole_setting.ADDR_IM920_CONTROL \
+            and params.command == whole_setting.COMMAND_START:
+            return True
+        else:
+            return False
+        
+    def exit(self) -> None:
+        sock2control = self.transceiver.create_socket(whole_setting.ADDR_IM920_CONTROL)
+        sock2control.send(Im920.encode("Child: Exit MissionStandbyNode."))
+        
